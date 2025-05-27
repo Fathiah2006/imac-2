5cb88607e0849dea76de079aa61cdcd7b3521e04
Emmanuel Olowu <51033541+zeddyemy@users.noreply.github.com>
2025-05-27 13:20:53 +0100

    create template files

---
+++ single.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package imac-france
+ */
+
+get_header();
+
+get_template_part('template-parts/single-page', get_post_format());
+
+get_footer();
